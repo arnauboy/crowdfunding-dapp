41a6d1b9f5f73fc20cfc1905fd7723ad659c5590
arnauboy <arnauboy@gmail.com>
2022-03-20 12:22:53 +0100

    Matic RPCs config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,29 +1,24 @@
-require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-waffle")
+const fs = require("fs")
+const privateKey = fs.readFileSync(".secret").toString()
+const projectId = "fc66d222860b9580c47e60a7a3a800d4b420dfd2"
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
-  solidity: "0.8.4",
   paths: {
     artifacts: './src/artifacts',
   },
   networks: {
     hardhat: {
       chainId: 1337
+    },
+    mumbai: {
+      url: `https://rpc-mumbai.maticvigil.com/v1/${projectId}`,
+      accounts: [privateKey]
+    },
+    mainnet: {
+      url: `https://rpc-mainnet.maticvigil.com/v1/${projectId}`,
+      accounts: [privateKey]
     }
-  }
+  },
+  solidity: "0.8.4"
 };
