9c1dc5ed93dd37e506edddb8273219ec89339920
arnauboy <arnauboy@gmail.com>
2022-03-22 13:24:10 +0100

    Font Tiillium and about img

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import me from '../images/me.jpg'
+import me from '../images/me.png'
 
 const About = () => {
   return (
@@ -8,10 +8,13 @@ const About = () => {
         <p style = {{  maxWidth: '50%',margin: 'auto',textAlign: 'jusitfy'}}>
           Over a year ago, I became interested in blockchain technology and all the possible services it could offer. As a computer science student, I thought it would be agreat
           idea to explore a little more and create a decentralized application on the Ethereum blockchain for my final thesis: Elefund.
-
-          Hope you enjoy!
         </p>
-        <img src ={me} alt="Arnau Garcia Rodríguez"/>
+        <h2 style={{textAlign: 'center'}}> Hope you enjoy! </h2>
+
+        <div>
+          <img style={{maxWidth: '20%', margin: 'auto', display: 'block', padding: '50px'}} src ={me} alt="Arnau Garcia Rodríguez" title="Me: Arnau Garcia"/>
+          <figcaption> Arnau Garcia</figcaption>
+        </div>
     </div>
   );
 };
