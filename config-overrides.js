df3a751bca2c00df40add6f1ffa9dfab345b3399
arnauboy <arnauboy@gmail.com>
2022-04-04 20:14:51 +0200

    Img stored in ipfs when submitted

---
+++ config-overrides.js
@@ -0,0 +1,18 @@
+const webpack = require('webpack');
+module.exports = function override(config, env) {
+    config.resolve.fallback = {
+        url: require.resolve('url'),
+        fs: require.resolve('fs'),
+        assert: require.resolve('assert'),
+        http: require.resolve('stream-http'),
+        https: require.resolve('https-browserify')
+    };
+    config.plugins.push(
+        new webpack.ProvidePlugin({
+            process: 'process/browser',
+            Buffer: ['buffer', 'Buffer'],
+        }),
+    );
+
+    return config;
+}
--- src/pages/startCampaign.js
+++ src/pages/startCampaign.js
@@ -1,8 +1,55 @@
-const StartCampaign = () => {
+import ipfs from '../ipfs'
+import React from  'react'
 
-  return (
-    <h1> Start Campaign</h1>
-  );
+
+class StartCampaign extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      ipfsHash: ''
+    }
+    this.captureFile = this.captureFile.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+
+  captureFile (event) {
+    event.preventDefault()
+    const file = event.target.files[0]
+    const reader =  new window.FileReader()
+    reader.readAsArrayBuffer(file)
+    reader.onloadend = () => {
+      this.setState({buffer: Buffer(reader.result)})
+      console.log(this.state.buffer, "holi")
+    }
+  }
+
+
+  onSubmit (event) {
+    event.preventDefault()
+    console.log('Submitting...')
+    ipfs.files.add(this.state.buffer, (error,result) => {
+      if(error){
+        console.error(error)
+        return
+      }
+      this.setState({ipfsHash: result[0].hash})
+      console.log('ipfsHash', this.state.ipfsHash)
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <h1> Start Campaign</h1>
+        <form onSubmit={this.onSubmit}>
+          <input type = "file" onChange={this.captureFile}/>
+          <input type = 'submit' />
+        </form>
+        <img src={`https://ipfs.io/ipfs/${this.state.ipfsHash}`} alt="" />
+      </div>
+    );
+  }
 };
 
 export default StartCampaign;
